45f30b34cc248a0cfceb63d5c19bfcd38b43e348
Jessica Natovich <jessicanatovich@yahoo.com>
2019-12-17 18:37:20 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,13 +1,7 @@
-class Book
-  attr_accessor :author, :page_count, :genre
-  attr_reader :title
-
-  def initialize(title)
-    @title = title
+def initialize(brand)
+    @brand = brand
+    BRANDS << brand if !BRANDS.include?(brand)
+    BRANDS << brand unless BRANDS.include?(brand)
   end
 
-  def turn_page
-    puts "Flipping the page...wow, you read fast!"
-  end
-
-end
+  def cobble
